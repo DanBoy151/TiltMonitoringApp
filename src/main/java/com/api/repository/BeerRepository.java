4ffe71d7b2b2eb20a55908c9048336e969f00262
danboy151 <dhorridge71@gmail.com>
2020-01-21 10:06:17 +0000

    dependency exception fix

--- src/main/java/com/api/repository/BeerRepository.java
+++ src/main/java/com/api/repository/BeerRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.api.model.Beer;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component("BeerRepository")
 public interface BeerRepository extends CrudRepository<Beer, Long>{
 	List<Beer> findByBeerName(String BeerName);
     List<Beer> findAll();
